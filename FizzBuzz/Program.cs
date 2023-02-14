498217f05331528abf18d20b7a5b2ee069ffad87
Skyler Adams <skyler14adams@gmail.com>
2023-02-14 12:07:45 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Reflection.Metadata;
+
+for (int idx = 1;idx <= 30;idx++)
+{
+   bool div3 = (idx % 3 == 0) ? true : false;
+    bool div5 = (idx % 5 == 0) ? true : false;
+    bool div3n5 = div3 && div5;
+    if (div3n5)
+    {
+        Console.WriteLine("FIZZBUZZ");
+        continue;
+    }
+    if(div5)
+    {
+        Console.WriteLine("BUZZ");
+        continue;
+    }
+    if(div3)
+    {
+        Console.WriteLine("FIZZ");
+        continue;
+    }
+    Console.WriteLine($"{idx}");
+}
